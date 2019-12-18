d624c275b36beb76f379a4aeada36347e2153879
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-12-18 11:41:33 +0900

    Flesh out prototype UI

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _controller;
+  List<int> _codepoints = const [];
+
+  @override
+  void initState() {
+    _controller = TextEditingController()..addListener(_textChanged);
+    super.initState();
+  }
+
+  void _textChanged() {
+    setState(() {
+      _codepoints = List.from(_controller.text.runes);
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,9 +56,27 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '"a" is available on Android SDK 10: ${android10BloomFilter.mightContain('a'.codeUnitAt(0))}',
+            TextField(
+              controller: _controller,
             ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: _codepoints.length,
+                itemBuilder: (context, i) {
+                  final cp = _codepoints[i];
+                  final available = android10BloomFilter.mightContain(cp);
+                  final message = available
+                      ? 'Available on Android 10'
+                      : 'Not available on Android 10';
+                  final icon = available
+                      ? const Icon(Icons.thumb_up)
+                      : const Icon(Icons.not_interested);
+                  return ListTile(
+                    leading: icon,
+                    title: Text(String.fromCharCode(cp)),
+                    trailing: Text(message),
+                  );
+                }),
           ],
         ),
       ),
