faf822a5fa0446a8316222b098af6c965101b2e4
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-12-21 22:58:35 +0900

    Move to card-based design

--- lib/text_analysis_page.dart
+++ lib/text_analysis_page.dart
@@ -40,51 +40,101 @@ class _TextAnalysisPageState extends State<TextAnalysisPage> {
       body: Center(
         child: Container(
           constraints: const BoxConstraints(maxWidth: 800),
-          padding: const EdgeInsets.all(32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
             children: <Widget>[
-              Expanded(
-                child: TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Input text to check here...',
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Is your text visible on mobile? Enter it here to see if any of it turns to "tofu".',
+                        style: Theme.of(context).textTheme.title,
+                      ),
+                      const SizedBox(height: 8),
+                      TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Input text to check here...',
+                        ),
+                        maxLines: 10,
+                        minLines: 5,
+                        controller: _controller,
+                        // This doesn't work right in Flutter Web: on mobile the
+                        // keyboard won't show up, but the field has focus and can't
+                        // be refocused so you can't force it to appear either.
+                        //autofocus: true,
+                      ),
+                    ],
                   ),
-                  maxLines: 50,
-                  minLines: 20,
-                  controller: _controller,
-                  style: Theme.of(context).textTheme.display1,
-                  // This doesn't work right in Flutter Web: on mobile the
-                  // keyboard won't show up, but the field has focus and can't
-                  // be refocused so you can't force it to appear either.
-                  //autofocus: true,
                 ),
               ),
-              const SizedBox(height: 32),
+              //const SizedBox(height: 32),
               if (!_analysis.isEmpty) ...[
-                Row(
-                  children: <Widget>[
-                    const Icon(Icons.android),
-                    const SizedBox(width: 16),
-                    Text(_analysis.androidSupportString),
-                    const Spacer(),
-                    const Icon(Icons.phone_iphone),
-                    const SizedBox(width: 16),
-                    Text(_analysis.iosSupportString)
-                  ],
-                ),
-                const SizedBox(height: 32),
-              ],
-              Expanded(
-                child: ListView.separated(
-                  itemCount: _analysis.uniqueCodePoints.length,
-                  separatorBuilder: (context, i) => const Divider(),
-                  itemBuilder: (context, i) => _CodePointTile(
-                    _analysis.analysisForIndex(i),
-                    key: ValueKey(i),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Overall Compatibility',
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Row(
+                                children: <Widget>[
+                                  const Icon(Icons.android),
+                                  const SizedBox(width: 8),
+                                  Text(_analysis.androidSupportString),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Row(
+                                children: <Widget>[
+                                  const Icon(Icons.phone_iphone),
+                                  const SizedBox(width: 8),
+                                  Text(_analysis.iosSupportString)
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+                Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          'Character Breakdown',
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      ListView.separated(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: _analysis.uniqueCodePoints.length,
+                        separatorBuilder: (context, i) => const Divider(),
+                        itemBuilder: (context, i) => _CodePointTile(
+                          _analysis.analysisForIndex(i),
+                          key: ValueKey(i),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ]
             ],
           ),
         ),
@@ -130,27 +180,32 @@ class _PlatformSupport extends StatelessWidget {
   final String ios;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const Icon(Icons.android),
-            const SizedBox(width: 16),
-            Text(android),
-          ],
-        ),
-        const SizedBox(height: 8),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const Icon(Icons.phone_iphone),
-            const SizedBox(width: 16),
-            Text(ios),
-          ],
-        ),
-      ],
+    final iconTheme = IconTheme.of(context);
+    return Theme(
+      data: Theme.of(context)
+          .copyWith(iconTheme: iconTheme.copyWith(size: iconTheme.size * 0.75)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Icon(Icons.android),
+              const SizedBox(width: 8),
+              Text(android, style: Theme.of(context).textTheme.overline),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Icon(Icons.phone_iphone),
+              const SizedBox(width: 8),
+              Text(ios, style: Theme.of(context).textTheme.overline),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
