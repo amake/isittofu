731e35f34a6ec61fab0146e95145fd9f7232e0ea
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-12-24 12:28:21 +0900

    Refactor TextAnalysisPage with Provider package

--- lib/text_analysis_page.dart
+++ lib/text_analysis_page.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:isittofu/analyzer.dart';
+import 'package:provider/provider.dart';
 
-class TextAnalysisPage extends StatefulWidget {
+class TextAnalysisModel extends ChangeNotifier {
+  String _text = '';
+
+  String get text => _text;
+
+  TextAnalysis _analysis = TextAnalysis.empty();
+
+  TextAnalysis get analysis => _analysis;
+
+  void setText(String text) {
+    if (_text != text) {
+      _text = text;
+      _analysis = const Analyzer().analyzeText(text);
+      notifyListeners();
+    }
+  }
+}
+
+class TextAnalysisPage extends StatelessWidget {
   const TextAnalysisPage({Key key}) : super(key: key);
 
   @override
-  _TextAnalysisPageState createState() => _TextAnalysisPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Is it tofu?'),
+      ),
+      body: ChangeNotifierProvider(
+        create: (context) => TextAnalysisModel(),
+        child: Center(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 800),
+            child: Builder(
+              builder: (context) => ListView(
+                children: <Widget>[
+                  const _TextInputCard(),
+                  //const SizedBox(height: 32),
+                  if (!Provider.of<TextAnalysisModel>(context)
+                      .analysis
+                      .isEmpty) ...[
+                    const _CompatibilitySummary(),
+                    const _CharacterBreakdown(),
+                  ]
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _TextAnalysisPageState extends State<TextAnalysisPage> {
+class _TextInputCard extends StatefulWidget {
+  const _TextInputCard({Key key}) : super(key: key);
+
+  @override
+  _TextInputCardState createState() => _TextInputCardState();
+}
+
+class _TextInputCardState extends State<_TextInputCard> {
   TextEditingController _controller;
-  TextAnalysis _analysis = TextAnalysis.empty();
 
   @override
   void initState() {
@@ -19,11 +72,8 @@ class _TextAnalysisPageState extends State<TextAnalysisPage> {
     super.initState();
   }
 
-  void _textChanged() {
-    setState(() {
-      _analysis = const Analyzer().analyzeText(_controller.text);
-    });
-  }
+  void _textChanged() =>
+      Provider.of<TextAnalysisModel>(context).setText(_controller.text);
 
   @override
   void dispose() {
@@ -33,114 +83,121 @@ class _TextAnalysisPageState extends State<TextAnalysisPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Is it tofu?'),
-      ),
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 800),
-          child: ListView(
-            children: <Widget>[
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Is your text visible on mobile? Enter it here to see if any of it turns to "tofu".',
-                        style: Theme.of(context).textTheme.title,
-                      ),
-                      const SizedBox(height: 8),
-                      TextField(
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Input text to check here...',
-                        ),
-                        maxLines: 10,
-                        minLines: 5,
-                        controller: _controller,
-                        // This doesn't work right in Flutter Web: on mobile the
-                        // keyboard won't show up, but the field has focus and can't
-                        // be refocused so you can't force it to appear either.
-                        //autofocus: true,
-                      ),
-                    ],
-                  ),
-                ),
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Is your text visible on mobile? Enter it here to see if any of it turns to "tofu".',
+              style: Theme.of(context).textTheme.title,
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Input text to check here...',
               ),
-              //const SizedBox(height: 32),
-              if (!_analysis.isEmpty) ...[
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Overall Compatibility',
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        const SizedBox(height: 8),
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Row(
-                                children: <Widget>[
-                                  const Icon(Icons.android),
-                                  const SizedBox(width: 8),
-                                  Text(_analysis.androidSupportString),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              child: Row(
-                                children: <Widget>[
-                                  const Icon(Icons.phone_iphone),
-                                  const SizedBox(width: 8),
-                                  Text(_analysis.iosSupportString)
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              maxLines: 10,
+              minLines: 5,
+              controller: _controller,
+              // This doesn't work right in Flutter Web: on mobile the
+              // keyboard won't show up, but the field has focus and can't
+              // be refocused so you can't force it to appear either.
+              //autofocus: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CompatibilitySummary extends StatelessWidget {
+  const _CompatibilitySummary({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Overall Compatibility',
+              style: Theme.of(context).textTheme.title,
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Row(
                     children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Text(
-                          'Character Breakdown',
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      ListView.separated(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: _analysis.sortedCodePoints.length,
-                        separatorBuilder: (context, i) => const Divider(),
-                        itemBuilder: (context, i) {
-                          final codePoint = _analysis.sortedCodePoints[i];
-                          return _CodePointTile(
-                            _analysis.analysis[codePoint],
-                            key: ValueKey(i),
-                          );
-                        },
-                      ),
+                      const Icon(Icons.android),
+                      const SizedBox(width: 8),
+                      Text(Provider.of<TextAnalysisModel>(context)
+                          .analysis
+                          .androidSupportString),
                     ],
                   ),
                 ),
-              ]
-            ],
-          ),
+                Expanded(
+                  child: Row(
+                    children: <Widget>[
+                      const Icon(Icons.phone_iphone),
+                      const SizedBox(width: 8),
+                      Text(Provider.of<TextAnalysisModel>(context)
+                          .analysis
+                          .iosSupportString)
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _CharacterBreakdown extends StatelessWidget {
+  const _CharacterBreakdown({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              'Character Breakdown',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          const SizedBox(height: 8),
+          ListView.separated(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: Provider.of<TextAnalysisModel>(context)
+                .analysis
+                .sortedCodePoints
+                .length,
+            separatorBuilder: (context, i) => const Divider(),
+            itemBuilder: (context, i) {
+              final analysis = Provider.of<TextAnalysisModel>(context).analysis;
+              final codePoint = analysis.sortedCodePoints[i];
+              return _CodePointTile(
+                analysis.analysis[codePoint],
+                key: ValueKey(i),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
@@ -181,6 +238,7 @@ class _PlatformSupport extends StatelessWidget {
 
   final String android;
   final String ios;
+
   @override
   Widget build(BuildContext context) {
     final iconTheme = IconTheme.of(context);
