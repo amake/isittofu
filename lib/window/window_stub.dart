64996a71746d1d4f063bc11121199d299ce980cb
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-12-25 23:41:17 +0900

    Remove unneeded mocks

--- lib/window/window_stub.dart
+++ lib/window/window_stub.dart
@@ -1,20 +1,7 @@
 Window window = Window();
 
 class Window {
-  History get history => History();
-  Location get location => Location();
-
-  // Mocks for custom extension functions
   String get decodedQuery => null;
 
   void setQuery(String _) {}
-}
-
-class History {
-  void pushState(Object _, Object __, Object ___) {}
-}
-
-class Location {
-  String get search => null;
-  String get hash => null;
 }
