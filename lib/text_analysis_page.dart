e1ec576ca6a0e65063d8f561e88425268e6162b4
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-12-28 21:19:28 +0900

    Add expandable help text

--- lib/text_analysis_page.dart
+++ lib/text_analysis_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:isittofu/analyzer.dart';
 import 'package:isittofu/window/window.dart';
 import 'package:provider/provider.dart';
@@ -168,17 +170,26 @@ class _CompatibilitySummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final analysis = Provider.of<TextAnalysisModel>(context).analysis;
+    final expanded = ValueNotifier(false);
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'Overall Compatibility',
-              style: Theme.of(context).textTheme.title,
+            Row(
+              children: <Widget>[
+                Text(
+                  'Overall Compatibility',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                _HelpToggleButton(expanded),
+              ],
             ),
-            const SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, bottom: 16),
+              child: _ExpandableHelpText(expanded),
+            ),
             Container(
               width: double.infinity,
               child: Wrap(
@@ -211,6 +222,107 @@ class _CompatibilitySummary extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _HelpToggleButton extends StatelessWidget {
+  const _HelpToggleButton(this.enabled, {Key key})
+      : assert(enabled != null),
+        super(key: key);
+
+  final ValueNotifier<bool> enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: ValueListenableBuilder<bool>(
+        valueListenable: enabled,
+        builder: (context, value, _) =>
+            value ? const Icon(Icons.help) : const Icon(Icons.help_outline),
+      ),
+      onPressed: () => enabled.value = !enabled.value,
+    );
+  }
+}
+
+const _kHelpHtml = 'These are the versions of iOS and Android that support all '
+    'of the characters in your text. The percentages are estimated from '
+    'data published by '
+    '<a href="https://developer.apple.com/support/app-store/">Apple</a> '
+    'and '
+    '<a href="https://developer.android.com/about/dashboards">Google</a> '
+    '(retrieved December 2019).';
+
+class _ExpandableHelpText extends StatelessWidget {
+  const _ExpandableHelpText(this.expanded, {Key key})
+      : assert(expanded != null),
+        super(key: key);
+
+  final ValueNotifier<bool> expanded;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: expanded,
+      builder: (context, value, child) => AnimatedCrossFade(
+        alignment: Alignment.centerLeft,
+        duration: const Duration(milliseconds: 200),
+        firstChild: child,
+        secondChild: const SizedBox.shrink(),
+        crossFadeState:
+            value ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Html(data: _kHelpHtml, onLinkTap: launch),
+          const SizedBox(height: 8),
+          Text('Legend', style: Theme.of(context).textTheme.subhead),
+          const SizedBox(height: 8),
+          const Padding(
+            padding: EdgeInsets.only(left: 8, bottom: 16),
+            child: _Legend(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Legend extends StatelessWidget {
+  const _Legend({Key key}) : super(key: key);
+
+  double get _scaleFactor => 0.6;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final iconTheme = IconTheme.of(context);
+    return Theme(
+      data: theme.copyWith(
+        iconTheme: iconTheme.copyWith(size: iconTheme.size * _scaleFactor),
+        textTheme: theme.textTheme.apply(fontSizeFactor: _scaleFactor),
+      ),
+      child: Column(
+        children: <Widget>[
+          Row(children: const <Widget>[
+            Icon(Icons.thumb_up, color: Colors.green),
+            SizedBox(width: 4),
+            Text('Supported by ≥70% of both iOS and Android devices')
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.warning, color: Colors.yellow),
+            SizedBox(width: 4),
+            Text('Supported by <70% of either iOS or Android devices')
+          ]),
+          Row(children: const <Widget>[
+            Icon(Icons.not_interested, color: Colors.red),
+            SizedBox(width: 4),
+            Text('Unsupported on iOS and/or Android')
+          ]),
+        ],
       ),
     );
   }
